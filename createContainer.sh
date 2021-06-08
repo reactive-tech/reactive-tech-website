fcac3386fd4c2e9cf3202eec503cd9f9c93b34b4
Alex Arica <alex.arica@reactive-tech.io>
2021-06-08 13:58:44 +0100

    Added a new deployment script to manage the prod and dev deployments. For dev, it deploys into a local Kubernetes cluster with ability to patch a prod image into a dev image and the ability to restart optionally if a Dev image changed.

--- createContainer.sh
+++
@@ -1,3 +0,0 @@
-gigo Gigo.yaml
-docker build -t reactivetechio/reactive-tech-website:latest .
-docker push reactivetechio/reactive-tech-website:latest
---
+++ deploy.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+source ~/source/kubernetes-cluster/local-cluster/util/deploymentUtilities.sh
+
+exitIfEnvArgIsMissing $1
+exitIfVersionArgIsMissing $2
+
+BUILD_ENV=$1
+BUILD_VERSION=$2
+REBUILD_DEV_IMAGE=$3
+
+BUILD_IMG="reactivetechio/reactive-tech-website:$BUILD_VERSION"
+BUILD_IMG_DEPLOYMENT_NAME="reactive-tech-website"
+BUILD_IMG_NAMESPACE="static-websites"
+
+if [ "$BUILD_ENV" == "dev" ]; then
+  BUILD_IMG="localhost:5000/$BUILD_IMG"
+fi
+
+echo "Deploying $BUILD_IMG in ${BUILD_ENV} environment"
+
+gigo deployment/Gigo.yaml
+cp deployment/Dockerfile /usr/share/dev-output/$BUILD_IMG_NAMESPACE/$BUILD_IMG_DEPLOYMENT_NAME
+
+if [ "$BUILD_ENV" == "prod" ]; then
+    buildAndDeployedDockerImg $BUILD_IMG $BUILD_IMG_DEPLOYMENT_NAME $BUILD_IMG_NAMESPACE
+
+elif [ "$BUILD_ENV" == "dev" ]; then
+
+  if ! isDevImageLocallyDeployed $BUILD_IMG $BUILD_IMG_DEPLOYMENT_NAME $BUILD_IMG_NAMESPACE; then
+
+    buildAndDeployedDockerImg $BUILD_IMG $BUILD_IMG_DEPLOYMENT_NAME $BUILD_IMG_NAMESPACE
+    echo "To deploy the DEV image, we are patching the Deployment resource '$BUILD_IMG_DEPLOYMENT_NAME' in the namespace: '$BUILD_IMG_NAMESPACE'"
+    kubectl patch deployment $BUILD_IMG_DEPLOYMENT_NAME -n $BUILD_IMG_NAMESPACE --patch "$(cat deployment/dev/deployment-patch.yaml)"
+
+  elif [ "$REBUILD_DEV_IMAGE" == "r" ]; then
+
+    buildAndDeployedDockerImg $BUILD_IMG $BUILD_IMG_DEPLOYMENT_NAME $BUILD_IMG_NAMESPACE
+    echo "To deploy the latest DEV image, we are restarting the Deployment resource '$BUILD_IMG_DEPLOYMENT_NAME' in the namespace: '$BUILD_IMG_NAMESPACE'"
+    kubectl rollout restart deployment $BUILD_IMG_DEPLOYMENT_NAME -n $BUILD_IMG_NAMESPACE
+  fi
+
+fi
