da55d9c6c5bfc497ff9a0085755d9ac19886d61f
Alex Arica <alex.arica@reactive-tech.io>
2023-12-10 11:30:06 +0000

    Updated the deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,15 @@
 
 source ~/source/kubernetes-cluster/deployment/app/baseDeploy.sh
 
-BUILD_IMG_DEPLOYMENT_NAME="reactive-tech-website"
-BUILD_IMG_NAMESPACE="static-websites"
+IMAGE_NAME="reactive-tech-website"
+IMAGE_PROJECT_NAME="reactivetech"
+KUBERNETES_NAMESPACE="static-websites"
 
 function buildAppAndCopyOutput() {
-  gigo deployment/Gigo.yaml
-  cp deployment/Dockerfile $BUILD_TARGET_PATH
-  cp -rp deployment/nginx $BUILD_TARGET_PATH
+  gigo gigo.yaml
+  cp -a target/. $BUILD_TARGET_PATH/image
+  cp -rp nginx $BUILD_TARGET_PATH
+  cp docker/Dockerfile $BUILD_TARGET_PATH
 }
 
 deploy
